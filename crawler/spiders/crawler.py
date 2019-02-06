382d7435c2d4554dba5bd6151f5ffba5b9838af9
yackoa <yackoa@gmail.com>
2019-02-06 14:38:20 +0000

    refactored to read input file name from console

--- crawler/spiders/crawler.py
+++ crawler/spiders/crawler.py
@@ -12,27 +12,28 @@ class MySpider(CrawlSpider):
 
     name = "test spider"
     invalid_chars_set = ["#", "?"]
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(current_dir, "input_domain_urls.txt")
 
-    with open(file_path, "rt") as f:
-        start_urls = [url.strip() for url in f.readlines()]
+    def __init__(self, urlList='',  *args, **kwargs ): #
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(current_dir, urlList)
 
-    allowed_domains =[]
-    for each_url in start_urls:
-        extracted_domain = tldextract.extract(each_url)
-        # extracts example.com -> www.example.com
-        allowed_domains.append(extracted_domain.domain + "." + extracted_domain.suffix)
+        with open(file_path, "rt") as f:
+            self.start_urls = [url.strip() for url in f.readlines()]
 
+        self.allowed_domains = []
+        for each_url in self.start_urls:
+            extracted_domain = tldextract.extract(each_url)
+            # extracts example.com from www.example.com
+            self.allowed_domains.append(extracted_domain.domain + "." + extracted_domain.suffix)
 
-    rules = (
-        Rule(LinkExtractor(), callback='parse_item', follow=True),
-    )
+        self.rules = (Rule(LinkExtractor(), callback='parse_item', follow=True),)
+        super().__init__(__rules=self.rules)
+        self.output_file = str(extracted_domain.domain) + ".txt"
 
     def parse_item(self, response):
         url = str(response.url)
         if self.is_valid(url):
-            with open("file.txt", "a") as infile:
+            with open(self.output_file, "a") as infile:
                 infile.write(url + '\n')
 
     def is_valid(self, url):
