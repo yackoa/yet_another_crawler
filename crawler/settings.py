2b4dbcc73451049e961d7e55f21d5c2bef467290
yackoa <yackoa@gmail.com>
2019-02-06 16:19:02 +0000

    changed throttling params to settings.py

--- crawler/settings.py
+++ crawler/settings.py
@@ -9,3 +9,18 @@ BOT_NAME = 'test'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 2
+
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 2
+
+# The download delay setting will honor only one of:
+CONCURRENT_REQUESTS_PER_DOMAIN = 2
+# CONCURRENT_REQUESTS_PER_IP = 16
+
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+AUTOTHROTTLE_ENABLED = True
