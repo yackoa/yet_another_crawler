2e254b62c3bcba2de84c2c594d730805b7f95b91
yackoa <yackoa@gmail.com>
2019-02-06 15:37:27 +0000

    added documentation

--- crawler/spiders/crawler.py
+++ crawler/spiders/crawler.py
@@ -10,33 +10,80 @@ from scrapy.linkextractors import LinkExtractor
 
 class MySpider(CrawlSpider):
 
-    name = "test spider"
+    name = "test_spider"
     invalid_chars_set = ["#", "?"]
 
     def __init__(self, urlList='',  *args, **kwargs ): #
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(current_dir, urlList)
 
-        with open(file_path, "rt") as f:
-            self.start_urls = [url.strip() for url in f.readlines()]
-
-        self.allowed_domains = []
-        for each_url in self.start_urls:
-            extracted_domain = tldextract.extract(each_url)
-            # extracts example.com from www.example.com
-            self.allowed_domains.append(extracted_domain.domain + "." + extracted_domain.suffix)
+        self.start_urls = self.get_start_urls(urlList)
+        self.allowed_domains = self.get_allowed_domains()
 
         self.rules = (Rule(LinkExtractor(), callback='parse_item', follow=True),)
         super().__init__(__rules=self.rules)
-        self.output_file = str(extracted_domain.domain) + ".txt"
+        self.output_file = str(self.allowed_domains[0]) + ".txt"
+
+    def get_start_urls(self, url_file):
+        """ reads the input file containing list of URLs and returns list of start urls
+
+        :param url_file:str
+            This is the file name
+        :return: [start_urls]
+            Returns return_list of start_urls
+
+        Example:
+            >>> get_start_urls([path_to_file])
+            ['https://www.example.com']
+        """
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(current_dir, url_file)
+
+        with open(file_path, "rt") as f:
+            start_urls = [url.strip() for url in f.readlines()]
+
+        return start_urls
+
+    def get_allowed_domains(self):
+        """" takes the list of start urls and returns the allowed domain list
+        :param None
+        :return: [allowed_domains]
+            Returns a returns list of allowed_domains
+
+
+        Example:
+            >>> get_start_urls(['https://www.example.com'])
+            ['example.com]
+        """
+        allowed_domains = []
+        for each_url in self.start_urls:
+            extracted_domain = tldextract.extract(each_url)
+            # extracts example.com from www.example.com
+            allowed_domains.append(extracted_domain.domain + "." + extracted_domain.suffix)
+        return allowed_domains
 
     def parse_item(self, response):
+        """ Takes each response asyncornosly from the crawler and appends to the file
+
+        :param response: response
+        :return: None
+        """
         url = str(response.url)
         if self.is_valid(url):
             with open(self.output_file, "a") as infile:
                 infile.write(url + '\n')
 
     def is_valid(self, url):
+        """ Checks to see if the URl doesn't have invalid characters mentioned in the charset
+
+        :param url: str
+        :return: Boolean
+
+        Example:
+            >>> get_start_urls(['https://www.example.com/share?=1'])
+            False
+            >>> get_start_urls(['https://www.example.com/some_page.html'])
+            True
+        """
         if any(char in self.invalid_chars_set for char in url):
             return False
 
